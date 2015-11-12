f7ed43af5c48b59aa9d212f1985b16e3bd75ee76
pMcFadyen <iltmhpat1157@gmail.com>
2015-11-12 07:36:03 -0600

    Fixed output time bug

--- app/build/generated/source/r/debug/p/m/logger/R.java
+++ app/build/generated/source/r/debug/p/m/logger/R.java
@@ -25,6 +25,7 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
         public static final int loggerbg=0x7f020001;
         public static final int loggerbg2=0x7f020002;
+        public static final int old=0x7f020003;
     }
     public static final class id {
         public static final int action_settings=0x7f0a0007;
